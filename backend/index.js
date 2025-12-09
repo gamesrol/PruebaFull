44867560c51f3dc1295a09f3b48cb4f8fbc3efc9
Gamesrol <gs.gore@gmail.com>
2025-12-09 21:26:05 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+
+// Variables de entorno
+const PORT = 3000;
+// const MONGODB_URI = 'mongodb://localhost:27017/dia25';
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Routers
+const indexRouter = require('./routers/indexRouters');
+const personsRouter = require('./routers/personsRouters');
+const classroomsRouter = require('./routers/classroomRouters');
+
+// Middlewares
+const logger = require('./middlewares/logs');
+const notFound = require('./middlewares/404');
+const internalServerError = require('./middlewares/500');
+const auth = require('./middlewares/auth');
+
+// Conexión a la base de datos
+mongoose.connect(MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
+
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+// Rutas
+app.use('/', indexRouter);
+app.use('/persons', personsRouter);
+app.use('/classrooms', classroomsRouter);
+
+// Middleware para rutas no encontradas
+app.use(notFound);
+app.use(internalServerError);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
